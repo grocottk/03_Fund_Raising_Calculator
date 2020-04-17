eebf48bee73c210ad52bff6e3db68ca313771fbc
grocottk <51727305+grocottk@users.noreply.github.com>
2020-04-17 17:09:12 +1200

    Completed Component 1
    This is the completed build of the first Component of the Fund Raising Calculator. Component 2 may be started at the beginning of next week

--- 01_item_list.py
+++ 01_item_list.py
@@ -6,3 +6,36 @@
 
 item_cost = []
 expenses = []
+
+# Get inputs and add to the mini list
+
+item = ""
+
+while item.lower() != "xxx":
+
+    item_cost = []
+
+    item = input("What is the name of the item? ")
+
+    # If the user enters the exit code, break the loop
+
+    if item.lower() == "xxx":
+
+        break
+
+    # Ask the user for the cost of the item
+    # (Eventually, replace this with a number checking function)
+
+    cost = float(input("How much does that item cost in NZD? "))
+
+    # Add both the item name and cost to the mini list
+
+    item_cost.append(item)
+    item_cost.append(cost)
+
+    # Add the mini lists to the master list
+
+    expenses.append(item_cost)
+
+
+print(expenses)
