c16c95da00fa2c355e8737d3f60635674cf7f885
grocottk <51727305+grocottk@users.noreply.github.com>
2020-04-20 13:15:43 +1200

    Completed Component 2
    This is the completed build of Component 2, which sorts the mini lists that are a part of the larger expenses list. The next Component that I will work on is Component 3, which finds the average of an array

--- 02_array_sorting.py
+++ 02_array_sorting.py
@@ -2,15 +2,15 @@
 # Fund Raising Calculator Component 2
 # Sorts the mini lists with the product name and price, that are a part of the larger list of all costs
 
-expenses = [["Nail", 3.0], ["Wooden Plank", 12.0], ["Rope", 7.0]]
+expenses = [["Nail", 3.0], ["Wooden Plank", 12.0], ["Rope", 7.5]]
 
 # To sort by cost:
 
-expenses.sort(key=lambda x: x[1])
+expenses.sort(key=lambda x: x[1], reverse=1)
 
 # Output:
 
-print("Items by cost (Highest to Lowet)")
+print("Items by cost (Highest to Lowest)")
 
 for item in expenses:
 
