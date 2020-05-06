bc675a1ddf37fc24c341fb5987de3f2c96692954
Kahlil <51727305+grocottk@users.noreply.github.com>
2020-05-07 09:55:32 +1200

    Component 8 Final Build
    This is the final build of Component 8, which calculates the profit and revenue required for a given project. This Component was made with pieces of code from two sources, both of which are given credit in the code itself. The next Component that I will begin work on is Component 9, which calculates the Profit Per Unit.

--- 08_profit_calculator.py
+++ 08_profit_calculator.py
@@ -1,4 +1,3 @@
-
 # Fund Raising Calculator Component 8
 # Asks the user how much profit they require (in either a percentage or amount), then add this to the total cost
 # ... to find the total sales needed
@@ -9,7 +8,6 @@
 
 
 def number_checker(question):
-
     error = "Please enter a number that is more than zero."
 
     valid = False
@@ -27,6 +25,12 @@ def number_checker(question):
 
         except ValueError:
             print(error)
+
+
+# Defining Variables:
+
+total = 1000
+
 
 # Main Routine:
 
@@ -53,20 +57,28 @@ amount_percentage = ["amount", "percentage"]
 profit_unit = string_checker("Would you like to enter your desired profit as "
                              "an amount or a percentage? ", amount_percentage)
 
-print(profit_unit)
-
 # End of Ms. Gottschalk's String Checker (Licenced under the 'GNU GENERAL PUBLIC LICENSE')
 
 # Checks to see which unit was chosen, then asks questions accordingly:
+
+# If the profit is as a percentage:
 
 if profit_unit == "percentage":
 
     percentage = number_checker("What percentage of profit would you like to make? ")
 
-    print(percentage)
+    # Multiplying Profit by the total, then adding to the total again (Credit to scanftree.com)
+
+    revenue_required = float((percentage / 100) * total) + float(total)
+
+# If the profit is as an amount:
 
 elif profit_unit == "amount":
 
     amount = number_checker("What amount of profit in NZD would you like to make? ")
 
-    print(amount)
+    # Add profit to total (Credit to scanftree.com)
+
+    revenue_required = float(amount) + float(total)
+
+print("The amount of revenue that you require to reach your profit goal is ${:.2f}".format(revenue_required))
